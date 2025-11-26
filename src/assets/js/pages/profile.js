01e5fc3ce3b04b540cd807ab5294fb3c14b2e330
thanhtuan2701 <20130456@st.hcmuaf.edu.vn>
2025-11-26 18:25:24 +0700

    20130456 nguyen thanh tuan (#11)
    * feat:add index.html, chore: add README, cau truc thu muc lam viec
    
    * làm trang đăng nhập
    
    * update components.js để hết lỗi 404
    
    * làm trang đăng ký
    
    * làm contact.html
    
    * làm contact.css
    
    * làm thêm trang quên mật khẩu và nút quên mật khẩu ở giao diện đăng nhập
    
    * làm trang events.html
    
    * làm trang events.css
    
    * test push
    
    * fix: update main with correct version
    
    * Cập nhật giao diện các trang contact, sign-in, sign-up
    
    * Code dung, luu truoc khi chuyen nhanh
    
    * thêm ảnh cho event và fix lại trang đăng nhập,đăng ký,quên mật khẩu theo yêu cầu của thầy
    
    * thêm chuyển hướng vào các nút
    
    * thêm trang đăng ký tham gia hoạt động
    
    * fix lại theo yêu cầu của thầy
    
    * sửa lại đường dẫn để dẫn đến trang event.html tại workshop, làm thêm trang đánh giá
    
    * thêm phần icon thiếu và chuyển hướng từ sign-in sang profile
    
    * làm trang hiển thị profile cho bé
    
    * thêm trang tài lieu
    
    * sửa header and footer
    
    * sửa lại header với thêm event vào nút bấm đánh giá đăng ký
    
    * thêm trang header.js
    
    * thêm thanh trạng thái user vào tất cả các trang
    
    * sửa lại xóa header.js và chỉ làm js tác dụng trạng thái đăng nhập cho mỗi trang profile
    
    * thay đổi ở header dòng dự án và làm lại trang material thành dự án stem
    
    * update trang profile và tách js ra riêng
    
    * sửa đổi trang tài liệu thành trang dự án stem
    
    ---------
    
    Co-authored-by: leee <23130355@st.hcmuaf.edu.vn>
    Co-authored-by: thanhtuan270102 <nhoktizike@gmail.com>

--- src/assets/js/pages/profile.js
+++ src/assets/js/pages/profile.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+ 
+    document.addEventListener("DOMContentLoaded", () => {
+    // Lấy thông tin user từ LocalStorage (sau khi đăng nhập)
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-
+    // xử lý ẩn hiện 2 trạng thái chưa và đã đăng nhập
     const headerLogin = document.querySelector(".header__user-login");
     const headerLogged = document.querySelector(".header__user-logged");
 
     if (userInfo) {
         headerLogin && (headerLogin.style.display = "none");
         headerLogged && (headerLogged.style.display = "flex");
-
+        // hiển thị tên user
         const nameText = document.querySelector(".user-short-name");
         if (nameText) nameText.textContent = userInfo.name;
     } else {
@@ -16,12 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-// Logout
-const logoutBtn = document.getElementById("logoutBtn");
-if (logoutBtn) {
-    logoutBtn.addEventListener("click", (e) => {
+// Xử lý Logout
+  const logoutBtn = document.getElementById("logoutBtn");
+  if (logoutBtn) {
+      logoutBtn.addEventListener("click", (e) => {
         e.preventDefault();
         localStorage.removeItem("userInfo");
         window.location.href = "/src/pages/user/sign-in.html";
-    });
+      });
 }
+
+
+    document.addEventListener("DOMContentLoaded", () => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    // Nếu chưa login → đẩy về trang đăng nhập
+    if (!user) {
+        window.location.href = "/src/pages/user/sign-in.html";
+    }
+});
